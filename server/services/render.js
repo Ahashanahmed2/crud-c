7df6955551a608cb0e59deb6533aba7431079422
ahashanahmed2 <ahashanahmed@yahoo.com>
2021-08-31 04:33:54 +0600

    crud-c-8

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 exports.homeRoute =(req,res)=>{
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://crud-c.herokuapp.com//api/users')
    
     .then(function(response){
         console.log(response);
